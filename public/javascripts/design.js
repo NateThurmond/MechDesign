b125e33ffbfc468577397468e6ce0532b0c4f6d6
NateThurmond <nathanthurmond@yahoo.com>
2024-12-08 18:08:05 -0600

    Added mongodb mechs as pickable options from list. Set up routes for same

--- public/javascripts/design.js
+++ public/javascripts/design.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+    /* List all mechs available for viewing */
+    $.getJSON("/design/mechs/list", (mechsToPick) => {
+        mechsToPick.forEach((mechToPick) => {
+            let {
+                _id,
+                mechs_armor,
+                // mechs_era,
+                mechs_introDate,
+                mechs_maxTonnage,
+                // mechs_mechID,
+                mechs_mechModel,
+                mechs_mechName,
+                // mechs_productionYear,
+                // mechs_techBase,
+                mechengine_mechWalk,
+            } = mechToPick;
+
+            $(".mechPickTable").append(
+                `<tr>
+                    <td><a href='/design/${_id}'>${mechs_mechName}</a></td>
+                    <td>${mechs_mechModel}</td>
+                    <td>${mechs_armor}</td>
+                    <td>${mechengine_mechWalk}</td>
+                    <td>${mechs_maxTonnage}</td>
+                    <td>${mechs_introDate}</td>
+                </tr>`
+            );
+        });
+    });
+
     // Only show button to view custom mechs if user is logged in
     if (getCookie("userName") == null || getCookie("userName") == "") {
         $("#link_1").css("display", "none");
@@ -7,28 +37,25 @@ $(document).ready(function () {
     }
 
     $("#mechSelector").on("click", function () {
-        let posVis = $(".popoutClass:visible").length;
-        if (posVis > 0) {
-            hidePopout();
-        } else {
+        let posVis = $(".popoutTab:visible").length;
+        hidePopout();
+        if (posVis === 0) {
             showPopout("popoutTab");
         }
     });
 
     $("#armorSelector").on("click", function () {
-        let posVis = $(".popoutClass:visible").length;
-        if (posVis > 0) {
-            hidePopout();
-        } else {
+        let posVis = $(".popoutTab2:visible").length;
+        hidePopout();
+        if (posVis === 0) {
             showPopout("popoutTab2");
         }
     });
 
     $("#weaponSelector").on("click", function () {
-        let posVis = $(".popoutClass:visible").length;
-        if (posVis > 0) {
-            hidePopout();
-        } else {
+        let posVis = $(".popoutTab3:visible").length;
+        hidePopout();
+        if (posVis === 0) {
             showPopout("popoutTab3");
         }
     });
